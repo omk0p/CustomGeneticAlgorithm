6b265618e7b33d2f8512e0a3232fe530493ea84f
omk0p <heremystuff@gmail.com>
2016-02-24 21:42:12 +0200

    Refactoring

---
+++ src/main/java/c/m/impl/FitnessFunction.java
@@ -0,0 +1,16 @@
+package c.m.impl;
+
+/**
+ * Created by Pavlo on 17.11.2015.
+ */
+public interface FitnessFunction {
+
+    long result(String bits, long[] input, long[] targets);
+
+    long[] sampleInputs();
+
+    long[] sampleTargets();
+
+    long[] outputs();
+}
+
