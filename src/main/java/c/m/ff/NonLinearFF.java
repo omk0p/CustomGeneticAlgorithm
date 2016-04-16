2750050c15ba4092f97cc87c0e7776366c529a02
omk0p <heremystuff@gmail.com>
2016-04-16 13:48:24 +0300

    Improved quality with 6 bits only.

--- src/main/java/c/m/ff/NonLinearFF.java
+++ src/main/java/c/m/ff/NonLinearFF.java
@@ -5,8 +5,10 @@ import c.m.utils.StringSplitter;
 import c.m.utils.Utils;
 
 public class NonLinearFF implements FitnessFunction {
-	private static final int GENOME_SIZE = 682;
-	int genomeParts = 62;
+	private static final int PART_BITS = 6;
+	private static final int GENOME_PARTS = 62;
+	private static final int GENOME_SIZE = GENOME_PARTS * (PART_BITS + 1);// sign
+																			// bit
 	double[] gNums;// genomeNumbers: weights,biases,etc.
 
 	@Override
@@ -19,9 +21,9 @@ public class NonLinearFF implements FitnessFunction {
 
 	@Override
 	public long evaluate(String bits, long[][] input, double[][] target) {
-		gNums = StringSplitter.signIntSplit(bits, genomeParts);
+		gNums = StringSplitter.signIntSplit(bits, GENOME_PARTS);
 		for (int i = 0; i < gNums.length; i++) {
-			gNums[i] = gNums[i] / 1000;
+			gNums[i] = gNums[i] / 100;
 		}
 
 		double[][] output = output();
@@ -43,14 +45,18 @@ public class NonLinearFF implements FitnessFunction {
 		double[][] t = { { -10, -10, -10, -10, -10, -10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5 },
 				{ -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 10, 10 } };
 
-		double[][] b1 = { { gNums[0] }, { gNums[1] }, { gNums[2] }, { gNums[3] }, { gNums[4] }, { gNums[5] }, { gNums[6] },
-				{ gNums[7] }, { gNums[8] }, { gNums[9] } };
-		double[][] IW = { { gNums[10], gNums[11], gNums[12] }, { gNums[13], gNums[14], gNums[15] }, { gNums[16], gNums[17], gNums[18] },
-				{ gNums[19], gNums[20], gNums[21] }, { gNums[22], gNums[23], gNums[24] }, { gNums[25], gNums[26], gNums[27] },
-				{ gNums[28], gNums[29], gNums[30] }, { gNums[31], gNums[32], gNums[33] }, { gNums[34], gNums[35], gNums[36] },
-				{ gNums[37], gNums[38], gNums[39] } };
-		double[][] LW = { { gNums[40], gNums[41], gNums[42], gNums[43], gNums[44], gNums[45], gNums[46], gNums[47], gNums[48], gNums[49] },
-				{ gNums[50], gNums[51], gNums[52], gNums[53], gNums[54], gNums[55], gNums[56], gNums[57], gNums[58], gNums[59] } };
+		double[][] b1 = { { gNums[0] }, { gNums[1] }, { gNums[2] }, { gNums[3] }, { gNums[4] }, { gNums[5] },
+				{ gNums[6] }, { gNums[7] }, { gNums[8] }, { gNums[9] } };
+		double[][] IW = { { gNums[10], gNums[11], gNums[12] }, { gNums[13], gNums[14], gNums[15] },
+				{ gNums[16], gNums[17], gNums[18] }, { gNums[19], gNums[20], gNums[21] },
+				{ gNums[22], gNums[23], gNums[24] }, { gNums[25], gNums[26], gNums[27] },
+				{ gNums[28], gNums[29], gNums[30] }, { gNums[31], gNums[32], gNums[33] },
+				{ gNums[34], gNums[35], gNums[36] }, { gNums[37], gNums[38], gNums[39] } };
+		double[][] LW = {
+				{ gNums[40], gNums[41], gNums[42], gNums[43], gNums[44], gNums[45], gNums[46], gNums[47], gNums[48],
+						gNums[49] },
+				{ gNums[50], gNums[51], gNums[52], gNums[53], gNums[54], gNums[55], gNums[56], gNums[57], gNums[58],
+						gNums[59] } };
 
 		double[][] b2 = { { gNums[60] }, { gNums[61] } };
 
@@ -66,14 +72,15 @@ public class NonLinearFF implements FitnessFunction {
 		// TODO min must be by row
 		double[][] y = reverseMapMinMax(yn, t);
 
-		//Utils.print("y", y);
-		//Utils.print("t", t);
+		// Utils.print("y", y);
+		// Utils.print("t", t);
 
 		return y;
 	}
-/*public static void main(String[] args) {
-	new NonLinearFF().output();
-}*/
+
+	/*
+	 * public static void main(String[] args) { new NonLinearFF().output(); }
+	 */
 	@Override
 	public int getGenomeSize() {
 		return GENOME_SIZE;
